fb4f88f7e75b43241c28d8c42e7c91501a210f96
ejmcculley <92233564+ejmcculley@users.noreply.github.com>
2022-04-25 11:41:53 -0700

    Add Park class with properties for Id, Name, Location, Type, and Description

---
+++ ParksLookup/Models/Park.cs
@@ -0,0 +1,12 @@
+namespace ParksLookup.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+
+  }
+}
